db6801d99e399e5c778022362e820210aa895857
Marios Kogias <marioskogias@gmail.com>
2014-09-05 14:24:56 +0300

    Update logging systems

---
+++ thesis/src/scribe.thrift
@@ -0,0 +1,14 @@
+enum ResultCode
+{
+OK,
+TRY_LATER
+}
+struct LogEntry
+{
+1: string category,
+2: string message
+}
+service scribe extends fb303.FacebookService
+{
+ResultCode Log(1: list<LogEntry> messages);
+}
